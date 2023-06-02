344ccb498b05432f497f52ffcea5dc77eb16c12d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-02 20:20:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Linux image builder based on [Buildroot](https://buildroot.org).
+
+```sh
+./build.sh [config]
+./build.sh qemu_x86_64
+```
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+CONFIG_FILE="$1"
+
+mkdir -p out
+cp config/"$CONFIG_FILE" out/.config
+
+if [ ! -d buildroot ]; then
+    git clone --depth 1 https://github.com/buildroot/buildroot
+fi
+cd buildroot
+
+make O=../out
