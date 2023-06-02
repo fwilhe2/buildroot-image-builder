1167e24bc787bb42a899d659b103cbccb99f8997
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-02 21:56:22 +0200

    Use relese of buildroot

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ mkdir -p out
 cp config/"$CONFIG_FILE" out/.config
 
 if [ ! -d buildroot ]; then
-    git clone --depth 1 https://github.com/buildroot/buildroot
+    git clone --depth 1 --branch 2023.02.1 https://github.com/buildroot/buildroot
 fi
 cd buildroot
 
