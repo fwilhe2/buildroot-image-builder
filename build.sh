340cda9ef7d000629b7381a2a1f5b27fb1270e13
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-02 23:15:26 +0200

    Improve script

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-CONFIG_FILE="$1"
+CONFIG_FILE="config/$1"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Usage: ./build.sh [config-file]"
+    exit 1
+fi
 
 mkdir -p out
-cp config/"$CONFIG_FILE" out/.config
+cp "$CONFIG_FILE" out/.config
 
 if [ ! -d buildroot ]; then
     git clone --depth 1 --branch 2023.02.1 https://github.com/buildroot/buildroot
